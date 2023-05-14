13a1db5fc46e1f1994304bd1d1f3af6da6711782
Steve Vermeulen <sfvermeulen@gmail.com>
2023-05-14 15:41:18 -0700

    Changed groups.lua.

--- lua/dracula/groups.lua
+++ lua/dracula/groups.lua
@@ -95,7 +95,7 @@ local function setup(configs)
       VertSplit = { fg = colors.black, },
       Folded = { fg = colors.comment, },
       FoldColumn = {},
-      Search = { fg = colors.black, bg = colors.orange, },
+      Search = { fg = colors.orange, bg = colors.selection, bold = true, underline = true, },
       IncSearch = { fg = colors.orange, bg = colors.comment, },
       LineNr = { fg = colors.comment, },
       MatchParen = { fg = colors.fg, underline = true, },
